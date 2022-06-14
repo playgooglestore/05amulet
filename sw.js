c0d71cf73c2034462146942e1755d06d94c349f3
playgooglestore <107193032+playgooglestore@users.noreply.github.com>
2022-06-14 17:03:32 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const dynamicCacheName = 'site-dynamic-v1';
+  
+// событие install
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+// событие activate
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+// событие fetch
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
