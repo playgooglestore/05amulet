cdba565029dffe3823fe777bd0429b2d9758fd26
playgooglestore <107193032+playgooglestore@users.noreply.github.com>
2022-06-14 17:04:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/04Planesp/sw.js')
+    navigator.serviceWorker.register('/05amulet/sw.js')
       .then(reg => console.log('service worker registered'))
       .catch(err => console.log('service worker not registered', err));
   }
